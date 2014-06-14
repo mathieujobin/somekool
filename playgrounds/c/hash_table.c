7162dd04b9078d7fa3e699f3315ed9502ad161e2
Mathieu Jobin <somekool@gmail.com>
2014-06-14 14:04:48 -0400

    add basic functions to print arrays + add/remove some debugging.

--- playgrounds/c/hash_table.c
+++ playgrounds/c/hash_table.c
@@ -1,13 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#define TRUE 1
+#define FALSE 0
 
+/*************** Simple Print functions *************************/
+void print_string_array(char **list, int size) {
+	printf("[ ");
+	int i; for (i = 0; i < size; i++) {
+		printf("%s, ", *(list+i));
+	}
+	printf("\x8\x8"); // erasing last delimitor
+	printf(" ]\n");
+}
+
+void print_num_array(int *list, int size) {
+	printf("[ ");
+	int i; for (i = 0; i < size; i++) {
+		printf("%d, ", *(list+i));
+	}
+	printf("\x8\x8"); // erasing last delimitor
+	printf(" ]\n");
+}
 
 /*************** File manipulation functions *********************/
 int file_length(FILE *fp) {
 	if (fseek(fp, 0L, SEEK_END)) { printf("error seeking to end...\n"); }
 	int sz = ftell(fp);
-	printf("DEBUG: file size is %d\n", sz);
+	printf("DEBUG: file size is %d\n", (int)sz);
 	fseek(fp, 0L, SEEK_SET);
 	return sz;
 }
@@ -38,7 +58,7 @@ struct list_of_words words_from_content(char *content) {
 	char *word = NULL;
 	int len = strlen(content);
 
-	printf("DEBUG: string is >>>%s<<<\n", content);
+	//printf("DEBUG: string is >>>%s<<<\n", content);
 	for (i = 0; i <= len; i++) {
 		char c = *(content+i);
 		// if we are on a delimitor char (comma, dot, space, tab, CR, LF)
@@ -59,12 +79,14 @@ struct list_of_words words_from_content(char *content) {
 			//printf("DEBUG: Char %d is %c (%d)\n", i, c, c);
 		}
 	}
+	printf("DEBUG: creating list of words\n");
 	struct list_of_words lw;
 	lw.word_count = word_count;
 	lw.list = malloc(sizeof(char*)*word_count); // reallocate needed amount of pointers
 	for(i = 0; i < word_count; i++) {
 		lw.list[i] = bucket[i];
 	}
+	printf("DEBUG: done copying, now freeing bucket\n");
 	free(bucket);
 	return lw;
 }
@@ -104,4 +126,7 @@ void main(void) {
 		key_list[i] = key_to_index(lw.list[i]);
 		printf("index for word %s is %d\n", lw.list[i], key_list[i]);
 	}
+	print_num_array(key_list, lw.word_count);
+	print_string_array(lw.list, lw.word_count);
+	free(key_list);
 }
